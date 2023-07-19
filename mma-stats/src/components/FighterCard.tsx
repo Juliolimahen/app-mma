6915bb223329348acdebb421823fba56704bb135
Juliolimahen <juliolima_henrique@outlook.com>
2023-07-19 16:01:34 -0300

    implement FighterList and FighterCard

---
+++ mma-stats/src/components/FighterCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Fighter {
+  name: string;
+  wins: number;
+  losses: number;
+  draws: number;
+  weightClass: string;
+}
+
+interface FighterCardProps {
+  fighter: Fighter;
+}
+
+const FighterCard: React.FC<FighterCardProps> = ({ fighter }) => {
+  return (
+    <div className="fighter-card">
+      <h2>{fighter.name}</h2>
+      <p>Wins: {fighter.wins}</p>
+      <p>Losses: {fighter.losses}</p>
+      <p>Draws: {fighter.draws}</p>
+      <p>Weight Class: {fighter.weightClass}</p>
+    </div>
+  );
+};
+
+export default FighterCard;
---
+++ mma-stats/src/components/FighterList.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import FighterCard from './FighterCard';
+
+interface Fighter {
+  name: string;
+  wins: number;
+  losses: number;
+  draws: number;
+  weightClass: string;
+}
+
+interface FighterListProps {
+  fighters: Fighter[];
+}
+
+const FighterList: React.FC<FighterListProps> = ({ fighters }) => {
+  return (
+    <div className="fighter-list">
+      {fighters.map((fighter) => (
+        <FighterCard key={fighter.name} fighter={fighter} />
+      ))}
+    </div>
+  );
+};
+
+export default FighterList;
